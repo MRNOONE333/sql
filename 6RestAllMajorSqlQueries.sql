c82e4081789fe5e3fa1422389a4304b437bde287
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-09-26 00:27:04 +0530

    Truncate / Practice ques.

--- 6RestAllMajorSqlQueries.sql
+++ 6RestAllMajorSqlQueries.sql
@@ -250,3 +250,39 @@ DROP COLUMN Stu_age;
 
 SELECT * FROM student;
 
+-- ----------------------------------------------------------------------------------------------------
+-- Truncate
+TRUNCATE TABLE student;
+
+CREATE TABLE student (
+    rollNo INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    marks INT NOT NULL,
+    grade VARCHAR(1),
+    city VARCHAR(20)
+);
+
+INSERT INTO student
+VALUES 
+    (101, "Anil", 75, "C", "Pune"),
+    (102, "Bhumika", 93, "A", "Mumbai"),
+    (103, "Chetan", 85, "A", "Delhi"),
+    (104, "Druv", 86, "B", "Mumbai"),
+    (105, "Emanuel", 12, "F", "Delhi"),
+    (106, "Farah", 82, "B", "Delhi");
+
+SELECT * FROM student;
+
+-- practice ques - 
+-- i-change name of column name to Full_name 
+-- ii-delete all students who scored marks less than 80
+-- iii-delete  the column of grade
+
+ALTER TABLE student
+CHANGE name Full_name VARCHAR(50);
+
+DELETE FROM student
+where marks<80;
+
+ALTER TABLE student 
+DROP COLUMN grade;
