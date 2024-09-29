69f07eeda6cf480deb7a19f9f0eeed29bcb824f6
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-09-29 11:44:23 +0530

    SQL sub-query

--- 6RestAllMajorSqlQueries.sql
+++ 6RestAllMajorSqlQueries.sql
@@ -407,3 +407,43 @@ SELECT name FROM employee;
 SELECT name FROM employee
 UNION ALL
 SELECT name FROM employee;
+
+-- SUB QUERY ----------------------------------------------------------------------------------------------------------------------------------
+-- name of all students who score moree than avg. marks
+DROP TABLE student;
+
+CREATE TABLE student (
+    rollNo INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    marks INT NOT NULL,
+    grade VARCHAR(1),
+    city VARCHAR(20)
+);
+
+
+INSERT INTO student
+VALUES 
+    (101, "Anil", 75, "C", "Pune"),
+    (102, "Bhumika", 93, "A", "Mumbai"),
+    (103, "Chetan", 85, "A", "Delhi"),
+    (104, "Druv", 86, "B", "Mumbai"),
+    (105, "Emanuel", 12, "F", "Delhi"),
+    (106, "Farah", 82, "B", "Delhi");
+
+SELECT * FROM student;
+
+-- sub query part1.
+SELECT AVG(marks)
+FROM student;
+
+-- hard code way.
+SELECT name, marks 
+FROM student 
+WHERE marks>72.1667;
+
+-- effifcent way, sub query
+SELECT name, marks 
+FROM student 
+WHERE marks>(
+SELECT AVG(marks)FROM student
+);
