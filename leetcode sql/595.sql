3e37d7c0b54bbca4aa64f3b4a196baf73f2790e8
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-10-08 23:25:00 +0530

    leetcode sql

---
+++ leetcode sql/595.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS CountryDB;
+USE CountryDB;
+
+CREATE TABLE IF NOT EXISTS World (
+    name VARCHAR(100) PRIMARY KEY,
+    continent VARCHAR(100),
+    area INT,
+    population INT,
+    gdp BIGINT
+);
+
+INSERT INTO World (name, continent, area, population, gdp) VALUES
+('Afghanistan', 'Asia', 652230, 25500100, 20343000000),
+('Albania', 'Europe', 28748, 2831741, 12960000000),
+('Algeria', 'Africa', 2381741, 37100000, 188681000000),
+('Andorra', 'Europe', 468, 78115, 3712000000),
+('Angola', 'Africa', 1246700, 20609294, 100990000000);
+
+SELECT name, population, area 
+from world 
+where area>= 3000000 or population >=25000000;
